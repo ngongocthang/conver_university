47cb97d3bd91197e2a282e934860a1f5fa12b255
ngongocthang <ngongocthang18082004@gmail.com>
2024-09-10 10:41:03 +0700

    change logo

--- wp-content/mu-plugins/university-post-type.php
+++ wp-content/mu-plugins/university-post-type.php
@@ -1,6 +1,7 @@
 <?php
 // Đăng ký các post type
-function university_post_types() {
+function university_post_types()
+{
     // Đăng ký post type cho sự kiện
     register_post_type('event', array(
         'labels' => array(
@@ -56,17 +57,17 @@ function university_post_types() {
         'menu_icon' => 'dashicons-format-aside',
     ));
 
-    // Đăng ký post type cho campuses
-    register_post_type('campuses', array(
+    // Đăng ký post type cho professors
+    register_post_type('professors', array(
         'labels' => array(
-            'name' => 'Campuses',
-            'singular_name' => 'Campus',
-            'add_new_item' => 'Add New Campus',
-            'edit_item' => 'Edit Campus',
-            'all_items' => 'All Campuses',
-            'view_item' => 'View Campus',
-            'not_found' => 'No Campuses found',
-            'not_found_in_trash' => 'No Campuses found in Trash'
+            'name' => 'Professors',
+            'singular_name' => 'Professors',
+            'add_new_item' => 'Add New Professors',
+            'edit_item' => 'Edit Professors',
+            'all_items' => 'All Professors',
+            'view_item' => 'View Professors',
+            'not_found' => 'No Professors found',
+            'not_found_in_trash' => 'No Professors found in Trash'
         ),
         'public' => true,
         'supports' => array('title', 'editor', 'thumbnail', 'excerpt'),
@@ -77,7 +78,8 @@ function university_post_types() {
 add_action('init', 'university_post_types');
 
 // Thêm taxonomy cho sự kiện
-function university_event_taxonomy() {
+function university_event_taxonomy()
+{
     register_taxonomy('event_category', 'event', array(
         'labels' => array(
             'name' => 'Event Categories',
@@ -100,12 +102,14 @@ function university_event_taxonomy() {
 add_action('init', 'university_event_taxonomy');
 
 // Thêm trường tùy chỉnh cho sự kiện
-function university_event_meta() {
+function university_event_meta()
+{
     add_meta_box('event_date', 'Event Date', 'university_event_date_callback', 'event', 'normal', 'high');
 }
 
 // Thêm trường tùy chỉnh cho slide
-function university_slide_meta() {
+function university_slide_meta()
+{
     add_meta_box('slide_info', 'Slide Information', 'university_slide_info_callback', 'slide', 'normal', 'high');
 }
 
@@ -113,24 +117,28 @@ add_action('add_meta_boxes', 'university_event_meta');
 add_action('add_meta_boxes', 'university_slide_meta');
 
 // Callback cho trường tùy chỉnh của sự kiện
-function university_event_date_callback($post) {
+function university_event_date_callback($post)
+{
     echo '<input type="date" name="event_date" value="' . get_post_meta($post->ID, 'event_date', true) . '">';
 }
 
 // Callback cho trường tùy chỉnh của slide
-function university_slide_info_callback($post) {
+function university_slide_info_callback($post)
+{
     echo '<input type="text" name="slide_info" value="' . get_post_meta($post->ID, 'slide_info', true) . '">';
 }
 
 // Hỗ trợ hình ảnh 
-function my_theme_setup() {
+function my_theme_setup()
+{
     add_theme_support('post-thumbnails');
 }
 
 add_action('after_setup_theme', 'my_theme_setup');
 
 // Lưu ngày sự kiện
-function save_event_date($post_id) {
+function save_event_date($post_id)
+{
     if (!isset($_POST['event_date']) || !current_user_can('edit_post', $post_id)) {
         return;
     }
@@ -142,25 +150,50 @@ function save_event_date($post_id) {
 
 add_action('save_post', 'save_event_date');
 
-// Thay đổi logo trang đăng nhập
-function my_custom_login_logo() {
-    echo '<style type="text/css">
-        h1 a {
-            background-image: url(' . get_stylesheet_directory_uri() . '/images/barksalot.jpg) !important;
-            background-size: cover !important;
-            width: 100px !important;
-            height: 100px !important;
-            border-radius: 50% !important;
-            display: block;
-        }
-    </style>';
+
+// Thêm tùy chọn logo vào Customizer
+function custom_login_customizer($wp_customize)
+{
+    $wp_customize->add_section('custom_login_section', array(
+        'title'    => __('Login Page', 'your-text-domain'),
+        'priority' => 30,
+    ));
+
+    $wp_customize->add_setting('custom_login_logo', array(
+        'default'   => '',
+        'transport' => 'refresh',
+    ));
+
+    $wp_customize->add_control(new WP_Customize_Image_Control($wp_customize, 'custom_login_logo_control', array(
+        'label'    => __('Login Logo', 'your-text-domain'),
+        'section'  => 'custom_login_section',
+        'settings' => 'custom_login_logo',
+    )));
 }
-add_action('login_enqueue_scripts', 'my_custom_login_logo');
+add_action('customize_register', 'custom_login_customizer');
 
-
-// Thay đổi đường link logo
-function my_custom_login_url() {
-    return 'https://www.facebook.com/ngongocthang18082004/';
+// Thay đổi logo trên trang đăng nhập
+function custom_login_logo()
+{
+    $logo = get_theme_mod('custom_login_logo', '');
+    if ($logo) {
+        echo '<style type="text/css">
+            #login h1 a {
+                background-image: url(' . esc_url($logo) . ');
+                background-size: cover !important;
+                width: 100px !important;
+                height: 100px !important;
+                border-radius: 50% !important;
+                display: block;
+            }
+        </style>';
+    }
 }
-add_filter('login_headerurl', 'my_custom_login_url');
+add_action('login_enqueue_scripts', 'custom_login_logo');
 
+// Thay đổi liên kết của logo
+function custom_login_url()
+{
+    return 'https://www.facebook.com/ngongocthang18082004/'; 
+}
+add_filter('login_headerurl', 'custom_login_url');
